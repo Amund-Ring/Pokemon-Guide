bd382c08f0857d8d2b1a2017e5c45a9c88988873
Amund Ring <60844467+Amund-Ring@users.noreply.github.com>
2024-09-14 12:28:51 +0200

    Initial Commit

---
+++ Pokemon Guide/Pokemon_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_GuideApp.swift
+//  Pokemon Guide
+//
+//  Created by Amund Ring on 14/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
